5257148d8f9f9fe3b133731b0537ac037aebd370
Allison <ayakosky@gmail.com>
2015-11-23 15:29:24 +0800

    edit signup and signin in "controller.js"

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,26 +6,39 @@ angular.module('app.controllers', [])
    
 .controller('login2Ctrl', function($scope, $state) {
     $scope.login = {};
-   
+     
     $scope.getLogin = function(){
-        console.log($scope.login);
-        if($scope.login.username == data.username
-            && $scope.login.password == data.password)
-        {
-            $state.go('tabsController.home');
-        }    
+        console.log($scope.login);    
+        Parse.User.logIn($scope.login.username, $scope.login.password, {
+            success: function(user) {
+                $state.go('tabsController.home')
+            },
+        error: function(user, error) {
+            alert("Error: " + error.code + " " + error.message);            }
+        });
     };
-    
-//     if(false){
-//         $state.go('tabsController.home');
-//     }
 })
    
 .controller('signupCtrl', function($scope) {
     $scope.signup = {};
-
+    
     $scope.getSignUp = function() {
         console.log($scope.signup);
+        var user = new Parse.User();
+        user.set("username", $scope.signup.username);
+        user.set("email", $scope.signup.email);
+        user.set("password", $scope.signup.password);
+        user.set("retype password", $scope.signup.retype);
+
+        user.signUp(null, {
+            success: function(user) {
+            // Hooray! Let them use the app now.
+            },
+            error: function(user, error) {
+            // Show the error message somewhere and let the user try again.
+            alert("Error: " + error.code + " " + error.message);
+            }
+        });
     }
 })
    
