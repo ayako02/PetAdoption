991eda1fdaa510ad3cace791f9a093aba61fd666
Allison <ayakosky@gmail.com>
2015-12-28 21:42:57 +0800

    controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers', [])
   
 .controller('loginCtrl', function($scope) {
-
+    
 })
    
 .controller('login2Ctrl', function($scope, $state) {
@@ -11,7 +11,7 @@ angular.module('app.controllers', [])
         console.log($scope.login);    
         Parse.User.logIn($scope.login.username, $scope.login.password, {
             success: function(user) {
-                $state.go('tabsController.home')
+                $state.go('tabsController.home');
             },
         error: function(user, error) {
             alert("Error: " + error.code + " " + error.message);            }
@@ -19,7 +19,7 @@ angular.module('app.controllers', [])
     };
 })
    
-.controller('signupCtrl', function($scope) {
+.controller('signupCtrl', function($scope, $state) {
     $scope.signup = {};
     
     $scope.getSignUp = function() {
@@ -33,6 +33,8 @@ angular.module('app.controllers', [])
         user.signUp(null, {
             success: function(user) {
             // Hooray! Let them use the app now.
+            alert("Hooray! Sign up sucessfully!");
+            $state.go('tabsController.home');
             },
             error: function(user, error) {
             // Show the error message somewhere and let the user try again.
@@ -42,23 +44,143 @@ angular.module('app.controllers', [])
     }
 })
    
-.controller('homeCtrl', function($scope) {
+.controller('homeCtrl', function($scope, $state, $ionicPopover) {
+    //popover-menu function
+    $ionicPopover.fromTemplateUrl('popover.html', {
+      scope: $scope
+    }).then(function(popover) {
+      $scope.popover = popover;
+    });
 
+    $scope.openPopover = function($event) {
+      $scope.popover.show($event);
+    };
+
+    $scope.closePopover = function() {
+      $scope.popover.hide();
+    };
+  
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.popover.remove();
+    });
+
+    $scope.logout =function(){
+        Parse.User.logOut();
+        $state.go('login');
+        $scope.closePopover();
+    }
 })
    
-.controller('searchCtrl', function($scope) {
+.controller('searchCtrl', function($scope, $state, $ionicPopover) {
+    //popover-menu function
+    $ionicPopover.fromTemplateUrl('popover.html', {
+        scope: $scope
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
 
+    $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+    };
+
+    $scope.closePopover = function() {
+        $scope.popover.hide();
+    };
+  
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+
+    $scope.logout =function(){
+        Parse.User.logOut();
+        $state.go('login');
+        $scope.closePopover();
+    }
 })
    
 .controller('newStatusCtrl', function($scope) {
+    $scope.status = {};
 
+    $scope.getNewStatus = function() {
+      console.log($scope.status);
+      var NewStatus = Parse.object.extends("NewStatus");
+      var status = new NewStatus();
+
+      status.set("PetName", $scope.status.petname);
+//       status.set("Age", 1);
+      status.set("breed", $scope.status.breed);
+      status.set("gender", $scope.status.gender);
+      status.set("location", $scope.status.location);
+
+      status.save(null, {
+        success: function(gameScore) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + gameScore.id);
+        },
+     error: function(gameScore, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+        }
+      });
+    }
 })
       
-.controller('notificationCtrl', function($scope) {
+.controller('notificationCtrl', function($scope, $state, $ionicPopover) {
+    //popover-menu function
+    $ionicPopover.fromTemplateUrl('popover.html', {
+        scope: $scope
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
 
+    $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+    };
+
+    $scope.closePopover = function() {
+        $scope.popover.hide();
+    };
+  
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+
+    $scope.logout =function(){
+        Parse.User.logOut();
+        $state.go('login');
+        $scope.closePopover();
+    }
 })
    
-.controller('profileCtrl', function($scope) {
+.controller('profileCtrl', function($scope, $state, $ionicPopover) {
+    //popover-menu function
+    $ionicPopover.fromTemplateUrl('popover.html', {
+        scope: $scope
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
 
+    $scope.openPopover = function($event) {
+        $scope.popover.show($event);
+    };
+
+    $scope.closePopover = function() {
+        $scope.popover.hide();
+    };
+  
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+
+    $scope.logout =function(){
+        Parse.User.logOut();
+        $state.go('login');
+        $scope.closePopover();
+    }
 })
  
